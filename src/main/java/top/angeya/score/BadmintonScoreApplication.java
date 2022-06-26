53a41d6ac7555f82b1b71f8f03239851184664c6
angeya <1571858518@qq.com>
2022-06-26 19:06:04 +0800

    上传代码

---
+++ src/main/java/top/angeya/score/BadmintonScoreApplication.java
@@ -0,0 +1,13 @@
+package top.angeya.score;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadmintonScoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BadmintonScoreApplication.class, args);
+    }
+
+}
